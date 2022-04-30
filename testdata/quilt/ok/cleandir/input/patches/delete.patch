openSUSE/rapidquilt

commit 7bc933b55c2c36494ac1bdf9bedbb4fab4b1ae1e

Never change current working directory
The current working directory is global state. Although it is in
fact OK to modify global state for a single run, it is known to
break parallel unit testing, and it may limit reusability of the
code in other projects.

Instead of changing the directory and using relative paths, make
sure to adjoin all relative paths to a base directory. AFAICS this
is an exhaustive list of all affected files:

- series file
- directory with patches (--patch-directory)
- files to be patched (input for patching)
- modified files (result of patching)
- quilt metadata (files under .pc)
  - applied-patches
  - per-patch backup files
- reject files

My goal was to keep user-visible relative (without the base
directory prefix).

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/rapidquilt/apply/common.rs
@@ -93,6 +93,7 @@ pub fn clean_empty_directories<P: AsRef<Path>, I: IntoIterator<Item = P>>(direct
 /// Save the `file` to disk. It also takes care of creating/deleting the file
 /// and containing directories.
 pub fn save_modified_file<'arena, H: BuildHasher>(
+    config: &ApplyConfig,
     filename: &Cow<'arena, Path>,
     file: &ModifiedFile,
     directories_for_cleaning: &mut HashSet<Cow<'arena, Path>, H>,
@@ -103,11 +104,12 @@ pub fn save_modified_file<'arena, H: BuildHasher>(
         println!("Saving modified file: {:?}: existed: {:?} deleted: {:?} len: {}", filename.as_ref(), file.existed, file.deleted, file.content.len());
     }
 
+    let file_path = config.base_dir.join(&filename);
     if file.existed {
         // If the file file existed, delete it. Whether we want to overwrite it
         // or really delete it - the file may be a hard link and we must replace
         // it with a new one, not edit the shared content.
-        match fs::remove_file(filename) {
+        match fs::remove_file(&file_path) {
             Ok(_) => {
                 // All is good.
             },
@@ -142,11 +144,11 @@ pub fn save_modified_file<'arena, H: BuildHasher>(
         if !file.existed {
             // If the file is new, the directory may be new as well. Let's
             // create it now.
-            if let Some(parent) = filename.parent() {
+            if let Some(parent) = file_path.parent() {
                 fs::create_dir_all(parent)?;
             }
         }
-        let mut output = File::create(filename)?;
+        let mut output = File::create(file_path)?;
 
         // If any patch set non-default permission, set them now
         if let Some(ref permissions) = file.permissions {
@@ -163,13 +165,14 @@ pub fn save_modified_file<'arena, H: BuildHasher>(
 /// the files and containing directories.
 pub fn save_modified_files<'arena, H: BuildHasher>
 (
+    config: &ApplyConfig,
     modified_files: &HashMap<Cow<'arena, Path>, ModifiedFile, H>,
     directories_for_cleaning: &mut HashSet<Cow<'arena, Path>, H>,
     verbosity: Verbosity)
     -> Result<(), Error>
 {
     for (filename, file) in modified_files {
-        save_modified_file(filename, file, directories_for_cleaning, verbosity)
+        save_modified_file(config, filename, file, directories_for_cleaning, verbosity)
             .with_context(|_| ApplyError::SaveModifiedFile { filename: filename.to_path_buf() })?;
     }
 
@@ -177,7 +180,8 @@ pub fn save_modified_files<'arena, H: BuildHasher>
 }
 
 /// Write the `original_file` as a quilt backup file.
-pub fn save_backup_file(patch_filename: &Path,
+pub fn save_backup_file(config: &ApplyConfig,
+                        patch_filename: &Path,
                         filename: &Path,
                         original_file: &ModifiedFile,
                         verbosity: Verbosity)
@@ -192,10 +196,12 @@ pub fn save_backup_file(patch_filename: &Path,
     }
 
     (|| -> Result<(), io::Error> { // TODO: Replace me with try-block when stable. (feature "try_blocks")
-        let path_parent = path.parent().unwrap(); // NOTE(unwrap): We know that there is a parent, we just built it ourselves.
+        let real_path = config.base_dir.join(&path);
+        // NOTE(unwrap): We know that there is a parent; we built it ourselves.
+        let path_parent = real_path.parent().unwrap();
 
         fs::create_dir_all(path_parent)?;
-        original_file.write_to(&mut File::create(&path)?)
+        original_file.write_to(&mut File::create(real_path)?)
     })().with_context(|_| ApplyError::SaveQuiltBackupFile { filename: path })?;
 
     Ok(())
@@ -237,6 +243,7 @@ pub struct PatchStatus<'arena, 'config> {
 ///
 /// Panics if both `old_filename` and `new_filename` are `None`.
 pub fn choose_filename_to_patch<'arena, 'filename, H: BuildHasher>(
+    config: &ApplyConfig,
     old_filename: Option<&'filename Cow<'arena, Path>>,
     new_filename: Option<&'filename Cow<'arena, Path>>,
     modified_files: &HashMap<Cow<'arena, Path>, ModifiedFile, H>)
@@ -257,7 +264,7 @@ pub fn choose_filename_to_patch<'arena, 'filename, H: BuildHasher>(
             match modified_files.get(old_filename) {
                 // File is not in the database yet, check what is on disk...
                 None => {
-                    if old_filename.exists() {
+                    if config.base_dir.join(old_filename).exists() {
                         // It exists on disk, lets use it!
                         old_filename
                     } else {
@@ -300,6 +307,7 @@ pub fn get_modified_file<
     'modified_files,
     H: BuildHasher>
 (
+    config: &ApplyConfig,
     filename: &Cow<'arena, Path>,
     modified_files: &'modified_files mut HashMap<Cow<'arena, Path>, ModifiedFile<'arena>, H>,
     arena: &'arena dyn Arena)
@@ -310,7 +318,7 @@ pub fn get_modified_file<
         Entry::Occupied(entry) => entry.into_mut(),
 
         Entry::Vacant(entry) => {
-            match arena.load_file(filename.as_ref()) {
+            match arena.load_file(&config.base_dir.join(filename)) {
                 Ok(data) => entry.insert(ModifiedFile::new(&data, true)),
 
                 // If the file doesn't exist, make empty one.
@@ -355,8 +363,8 @@ pub fn apply_one_file_patch<
     -> Result<bool, Error>
 {
     // Get the file to patch
-    let target_filename = choose_filename_to_patch(file_patch.old_filename(), file_patch.new_filename(), modified_files).clone();
-    let file = get_modified_file(&target_filename, modified_files, arena)
+    let target_filename = choose_filename_to_patch(config, file_patch.old_filename(), file_patch.new_filename(), modified_files).clone();
+    let file = get_modified_file(config, &target_filename, modified_files, arena)
         .with_context(|_| ApplyError::LoadFileToPatch { filename: target_filename.to_path_buf() })?;
 
     // If the patch renames the file. do it now...
@@ -375,7 +383,7 @@ pub fn apply_one_file_patch<
         // to do later - unless something else changes it, we will need to delete it from disk.
         let mut tmp_file = file.move_out();
 
-        let new_file = get_modified_file(new_filename, modified_files, arena)
+        let new_file = get_modified_file(config, new_filename, modified_files, arena)
             .with_context(|_| ApplyError::LoadFileToPatch { filename: new_filename.to_path_buf() })?;
 
         if !new_file.move_in(&mut tmp_file) {
@@ -389,7 +397,7 @@ pub fn apply_one_file_patch<
                      new_filename.display());
 
             // Put the content back to the old file.
-            let file = get_modified_file(&target_filename, modified_files, arena)
+            let file = get_modified_file(config, &target_filename, modified_files, arena)
                 .with_context(|_| ApplyError::LoadFileToPatch { filename: target_filename.to_path_buf() })?;
             file.move_in(&mut tmp_file);
 
@@ -460,6 +468,7 @@ pub fn rollback_applied_patch<'arena, 'modified_files, H: BuildHasher>(
 /// Rolls back all `FilePatch`es belonging to the `rejected_patch_index` and save
 /// ".rej" files for each `FilePatch` that failed applying
 pub fn rollback_and_save_rej_files<'arena, H: BuildHasher>(
+    config: &ApplyConfig,
     applied_patches: &mut Vec<PatchStatus<'arena, '_>>,
     modified_files: &mut HashMap<Cow<'arena, Path>, ModifiedFile<'arena>, H>,
     rejected_patch_index: usize,
@@ -477,7 +486,7 @@ pub fn rollback_and_save_rej_files<'arena, H: BuildHasher>(
         if applied_patch.report.failed() {
             let rej_filename = make_rej_filename(&applied_patch.target_filename);
 
-            let file = match File::create(&rej_filename) {
+            let file = match File::create(config.base_dir.join(&rej_filename)) {
                 Ok(file) => {
                     file
                 }
@@ -519,6 +528,7 @@ pub fn rollback_and_save_rej_files<'arena, H: BuildHasher>(
 /// Rolls back all `FilePatch`es up to the one belonging to patch with
 /// `down_to_index` index and generates quilt backup files for all of them.
 pub fn rollback_and_save_backup_files<'arena, H: BuildHasher>(
+    config: &ApplyConfig,
     applied_patches: &mut Vec<PatchStatus<'arena, '_>>,
     modified_files: &mut HashMap<Cow<'arena, Path>, ModifiedFile<'arena>, H>,
     down_to_index: usize,
@@ -532,12 +542,12 @@ pub fn rollback_and_save_backup_files<'arena, H: BuildHasher>(
 
         let file = rollback_applied_patch(applied_patch, modified_files);
 
-        save_backup_file(&applied_patch.patch_filename, &applied_patch.target_filename, &file, verbosity)?;
+        save_backup_file(config, &applied_patch.patch_filename, &applied_patch.target_filename, &file, verbosity)?;
 
         if let Some(new_filename) = applied_patch.file_patch.new_filename() {
             // If it was a rename, we also have to backup the new file (it will be empty file).
             let new_file = modified_files.get(new_filename).unwrap(); // NOTE(unwrap): It must be there, we must have loaded it when applying the patch.
-            save_backup_file(applied_patch.patch_filename, new_filename, &new_file, verbosity)?;
+            save_backup_file(config, applied_patch.patch_filename, new_filename, &new_file, verbosity)?;
         }
     }
 

src/rapidquilt/apply/parallel.rs
@@ -319,7 +319,7 @@ fn apply_worker_task<'arena, 'config, BroadcastFn: Fn(Message)> (
     // If this is not dry-run, save all the results
     if !config.dry_run {
         // Rollback the last applied patch and generate .rej files if any
-        if let Err(err) = rollback_and_save_rej_files(&mut applied_patches, &mut modified_files, earliest_broken_patch_index, config.verbosity) {
+        if let Err(err) = rollback_and_save_rej_files(config, &mut applied_patches, &mut modified_files, earliest_broken_patch_index, config.verbosity) {
             barrier.wait(); // We're forced to wait on the barrier before leaving, if not, the rest of threads will hang
             return Err(err);
         }
@@ -330,7 +330,7 @@ fn apply_worker_task<'arena, 'config, BroadcastFn: Fn(Message)> (
 
         // Save all the files we modified
         let mut directories_for_cleaning = HashSet::with_hasher(BuildHasherDefault::<seahash::SeaHasher>::default());
-        if let Err(err) = save_modified_files(&modified_files, &mut directories_for_cleaning, config.verbosity) {
+        if let Err(err) = save_modified_files(config, &modified_files, &mut directories_for_cleaning, config.verbosity) {
             barrier.wait(); // We're forced to wait on the barrier before leaving, if not, the rest of threads will hang
             return Err(err);
         }
@@ -357,7 +357,7 @@ fn apply_worker_task<'arena, 'config, BroadcastFn: Fn(Message)> (
                 ApplyConfigBackupCount::Last(n) => if final_patch > n { final_patch - n } else { 0 },
             };
 
-            rollback_and_save_backup_files(&mut applied_patches, &mut modified_files, down_to_index, config.verbosity)?;
+            rollback_and_save_backup_files(config, &mut applied_patches, &mut modified_files, down_to_index, config.verbosity)?;
         }
     }
 

src/rapidquilt/apply/sequential.rs
@@ -81,7 +81,7 @@ pub fn apply_patches<'a, 'arena>(config: &'a ApplyConfig, arena: &'arena dyn Are
             analyze_patch_failure(config.verbosity, index, &applied_patches, &modified_files, &mut failure_analysis)?;
 
             if !config.dry_run {
-                rollback_and_save_rej_files(&mut applied_patches, &mut modified_files, index, config.verbosity)?;
+                rollback_and_save_rej_files(config, &mut applied_patches, &mut modified_files, index, config.verbosity)?;
             }
 
             break;
@@ -94,7 +94,7 @@ pub fn apply_patches<'a, 'arena>(config: &'a ApplyConfig, arena: &'arena dyn Are
         }
 
         let mut directories_for_cleaning = HashSet::with_hasher(BuildHasherDefault::<seahash::SeaHasher>::default());
-        save_modified_files(&modified_files, &mut directories_for_cleaning, config.verbosity)?;
+        save_modified_files(config, &modified_files, &mut directories_for_cleaning, config.verbosity)?;
         clean_empty_directories(directories_for_cleaning)?;
 
         if config.do_backups == ApplyConfigDoBackups::Always ||
@@ -110,7 +110,7 @@ pub fn apply_patches<'a, 'arena>(config: &'a ApplyConfig, arena: &'arena dyn Are
                 ApplyConfigBackupCount::Last(n) => if final_patch > n { final_patch - n } else { 0 },
             };
 
-            rollback_and_save_backup_files(&mut applied_patches, &mut modified_files, down_to_index, config.verbosity)?;
+            rollback_and_save_backup_files(config, &mut applied_patches, &mut modified_files, down_to_index, config.verbosity)?;
         }
     }
 

src/rapidquilt/apply/mod.rs
@@ -55,6 +55,7 @@ pub struct SeriesPatch {
 
 #[derive(Debug)]
 pub struct ApplyConfig<'a> {
+    pub base_dir: &'a Path,
     pub series_patches: &'a [SeriesPatch],
     pub patches_path: &'a Path,
     pub fuzz: usize,

src/rapidquilt/cmd.rs
@@ -114,9 +114,10 @@ fn read_series_file<P: AsRef<Path>>(series_path: P) -> Result<Vec<SeriesPatch>, 
         }).collect()
 }
 
-fn save_applied_patches(applied_patches: &[SeriesPatch]) -> Result<(), Error> {
-    fs::create_dir_all(".pc")?;
-    let mut file_applied_patches = BufWriter::new(fs::OpenOptions::new().create(true).append(true).open(".pc/applied-patches")?);
+fn save_applied_patches(config: &ApplyConfig, applied_patches: &[SeriesPatch]) -> Result<(), Error> {
+    let quilt_pc = config.base_dir.join(".pc");
+    fs::create_dir_all(&quilt_pc)?;
+    let mut file_applied_patches = BufWriter::new(fs::OpenOptions::new().create(true).append(true).open(quilt_pc.join("applied-patches"))?);
     for applied_patch in applied_patches {
         writeln!(file_applied_patches, "{}", applied_patch.filename.display())?;
     }
@@ -133,6 +134,9 @@ enum PushGoal {
 fn cmd_push<'a, F: Iterator<Item = &'a String>>(matches: &Matches, mut free_args: F, verbosity: Verbosity) -> Result<bool, Error>
 {
     // Parse "push" specific arguments
+    let base_dir = matches.opt_str("directory").unwrap_or_default();
+    let base_dir = Path::new(&base_dir);
+
     let do_backups = match matches.opt_str("backup") {
         Some(ref s) if s == "always" => ApplyConfigDoBackups::Always,
         Some(ref s) if s == "onfail" => ApplyConfigDoBackups::OnFail,
@@ -147,7 +151,8 @@ fn cmd_push<'a, F: Iterator<Item = &'a String>>(matches: &Matches, mut free_args
         None                      => ApplyConfigBackupCount::Last(100),
     };
 
-    let patches_path = matches.opt_str("p").unwrap_or_else(|| "patches".to_string());
+    let patches_path = base_dir.join(
+        if let Some(ref s) = matches.opt_str("p") { s } else { "patches" });
 
     let fuzz = matches.opt_str("fuzz").and_then(|n| n.parse::<usize>().ok()).unwrap_or(0);
 
@@ -177,11 +182,11 @@ fn cmd_push<'a, F: Iterator<Item = &'a String>>(matches: &Matches, mut free_args
     }
 
     // Process series file
-    let series_patches = read_series_file("series")
+    let series_patches = read_series_file(base_dir.join("series"))
         .with_context(|_| "When reading \"series\" file.")?;
 
     // Determine the last patch
-    let first_patch = if let Ok(applied_patch_filenames) = read_series_file(".pc/applied-patches") {
+    let first_patch = if let Ok(applied_patch_filenames) = read_series_file(base_dir.join(".pc/applied-patches")) {
         for (p1, p2) in series_patches.iter().zip(applied_patch_filenames.iter()) {
             if p1.filename != p2.filename {
                 return Err(format_err!("There is mismatch in \"series\" and \".pc/applied-patches\" files! {} vs {}", p1.filename.display(), p2.filename.display()));
@@ -217,6 +222,7 @@ fn cmd_push<'a, F: Iterator<Item = &'a String>>(matches: &Matches, mut free_args
     let series_patches = &series_patches[first_patch..last_patch];
 
     let config = ApplyConfig {
+        base_dir,
         series_patches,
         patches_path: patches_path.as_ref(),
         fuzz,
@@ -244,7 +250,7 @@ fn cmd_push<'a, F: Iterator<Item = &'a String>>(matches: &Matches, mut free_args
     };
 
     if !config.dry_run {
-        save_applied_patches(&config.series_patches[0..apply_result.applied_patches])
+        save_applied_patches(&config, &config.series_patches[0..apply_result.applied_patches])
             .with_context(|_| "When saving applied patches.")?;
     }
 
@@ -337,10 +343,6 @@ pub fn run<A: IntoIterator>(args: A) -> Result<bool, Error> where A::Item: AsRef
     } else {
         Verbosity::Normal
     };
-
-    if let Some(directory) = matches.opt_str("directory") {
-        env::set_current_dir(&directory).with_context(|_| format!("Changing current directory to \"{}\"", directory))?;
-    }
 
     if let Some(manual_threads) = env::var("RAPIDQUILT_THREADS").ok().and_then(|value_txt| value_txt.parse().ok()) {
         rayon::ThreadPoolBuilder::new().num_threads(manual_threads).build_global()?;

commit c15528d8ef3598be10ab2fd5b0a2a21f37061bbc

Fix removal of empty directories
Since paths to patched files may not be relative to the current
working directory, this check for empty directories must also use
the base directory as a prefix.

Fixes: 7bc933b55c2c36494ac1bdf9bedbb4fab4b1ae1e

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

src/rapidquilt/apply/common.rs
@@ -35,7 +35,11 @@ pub fn make_rej_filename<P: AsRef<Path>>(path: P) -> PathBuf {
 }
 
 /// Delete all directories and their parents if they are empty.
-pub fn clean_empty_directories<P: AsRef<Path>, I: IntoIterator<Item = P>>(directories_for_cleaning: I) -> Result<(), io::Error> {
+pub fn clean_empty_directories<P, I>(base_dir: P, directories_for_cleaning: I) -> Result<(), io::Error>
+where P: AsRef<Path>,
+      I: IntoIterator,
+      I::Item: AsRef<Path>
+{
     // Warning: This function can be called by multiple threads at the same time for the same
     //          directories (or nested directories), so things may disappear under its hands, it
     //          must be able to deal with it.
@@ -47,8 +51,9 @@ pub fn clean_empty_directories<P: AsRef<Path>, I: IntoIterator<Item = P>>(direct
         // the working directory)
         let mut directory = directory.as_ref();
         loop {
+            let dir_path = base_dir.as_ref().join(&directory);
             // Check if there is at least one file in the directory
-            match fs::read_dir(directory).map(|mut d| d.next()) {
+            match fs::read_dir(&dir_path).map(|mut d| d.next()) {
                 Err(ref error) if error.kind() == io::ErrorKind::NotFound => {
                     // Directory itself does not exist, we are done.
                     // This may happen if we already deleted it when cleaning after another file.
@@ -73,7 +78,7 @@ pub fn clean_empty_directories<P: AsRef<Path>, I: IntoIterator<Item = P>>(direct
                 }
             }
 
-            match fs::remove_dir(directory) {
+            match fs::remove_dir(dir_path) {
                 Err(error) => {
                     if error.kind() != io::ErrorKind::NotFound {
                         return Err(error);

src/rapidquilt/apply/parallel.rs
@@ -262,7 +262,7 @@ fn save_files_worker<'arena, 'config> (
         if let Err(err) = state.modified_files.save(&mut directories_for_cleaning) {
             return Err(err);
         }
-        clean_empty_directories(directories_for_cleaning)?;
+        clean_empty_directories(config.base_dir, directories_for_cleaning)?;
 
         // Maybe save some backup files
         if config.do_backups == ApplyConfigDoBackups::Always ||

src/rapidquilt/apply/sequential.rs
@@ -84,7 +84,7 @@ pub fn apply_patches<'a, 'arena>(config: &'a ApplyConfig, arena: &'arena dyn Are
 
         let mut directories_for_cleaning = HashSet::with_hasher(BuildHasherDefault::<seahash::SeaHasher>::default());
         state.modified_files.save(&mut directories_for_cleaning)?;
-        clean_empty_directories(directories_for_cleaning)?;
+        clean_empty_directories(config.base_dir, directories_for_cleaning)?;
 
         if config.do_backups == ApplyConfigDoBackups::Always ||
           (config.do_backups == ApplyConfigDoBackups::OnFail &&

testdata/quilt/ok/cleandir/expect/.pc/applied-patches
@@ -1,0 +1,1 @@
+delete.patch

testdata/quilt/ok/cleandir/expect/.pc/delete.patch/subdir/to-delete
@@ -1,0 +1,1 @@
+This file will be deleted.

testdata/quilt/ok/cleandir/expect/patches/delete.patch
@@ -1,0 +1,8 @@
+---
+ subdir/to-delete |    1 -
+ 1 file changed, 1 deletion(-)
+
+--- a/subdir/to-delete
++++ /dev/null
+@@ -1 +0,0 @@
+-This file will be deleted.

testdata/quilt/ok/cleandir/expect/series
@@ -1,0 +1,1 @@
+delete.patch

testdata/quilt/ok/cleandir/input/patches/delete.patch
@@ -1,0 +1,8 @@
+---
+ subdir/to-delete |    1 -
+ 1 file changed, 1 deletion(-)
+
+--- a/subdir/to-delete
++++ /dev/null
+@@ -1 +0,0 @@
+-This file will be deleted.

testdata/quilt/ok/cleandir/input/series
@@ -1,0 +1,1 @@
+delete.patch

testdata/quilt/ok/cleandir/input/subdir/to-delete
@@ -1,0 +1,1 @@
+This file will be deleted.